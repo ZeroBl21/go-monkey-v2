49e8abbb5e36ca5b6c80196061a71cfd2a02fcda
Darwin Perdomo <ZeroBl@hotmail.com>
2024-05-20 20:14:48 -0400

    feat(parser/): Add basic parser and parsing of let stataments

---
+++ src/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/ZeroBl21/go-monkey/src/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token // The token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (s *LetStatement) statementNode()       {}
+func (s *LetStatement) TokenLiteral() string { return s.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
