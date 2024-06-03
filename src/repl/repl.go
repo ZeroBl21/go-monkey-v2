8545c931ff2646c9033c1d3f07970dcbdb4a1bff
Darwin Perdomo <ZeroBl@hotmail.com>
2024-06-03 12:33:11 -0400

    feat(repl/): Adding the 'e' to repl

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ZeroBl21/go-monkey/src/evaluator"
 	"github.com/ZeroBl21/go-monkey/src/lexer"
 	"github.com/ZeroBl21/go-monkey/src/parser"
 )
@@ -35,8 +36,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
