874473fcd0ee28a6f936c0a873f190ce0652bc14
Darwin Perdomo <ZeroBl@hotmail.com>
2024-06-03 13:52:10 -0400

    feat(evaluator/): Add minus prefix operator evaluation and update tests

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -37,6 +37,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -52,6 +54,17 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{
+		Value: -value,
 	}
 }
 
