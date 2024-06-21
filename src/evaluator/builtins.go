83b19426e8ccf59052324555f7331a6f19b2b4f0
Darwin Perdomo <ZeroBl@hotmail.com>
2024-06-21 14:11:23 -0400

    feat(evaluator/builtins.go): Add `push` function

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -89,9 +89,30 @@ func _restFn(args ...object.Object) object.Object {
 	return NULL
 }
 
+func _pushFn(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of arguments. got=%d, want=2",
+			len(args))
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `push` must be ARRAY, got %s",
+			args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Elements)
+
+	newElements := make([]object.Object, length+1, length+1)
+	copy(newElements, arr.Elements)
+	newElements[length] = args[1]
+
+	return &object.Array{Elements: newElements}
+}
+
 func init() {
 	RegisterBuiltin("len", _lenFn)
 	RegisterBuiltin("unicodeLen", _unicodeLenFn)
 	RegisterBuiltin("last", _lastFn)
 	RegisterBuiltin("rest", _restFn)
+	RegisterBuiltin("push", _pushFn)
 }
