e2ed79836313fd2aa11f4cf165faa9508adcf76f
Darwin Perdomo <ZeroBl@hotmail.com>
2024-06-21 13:24:44 -0400

    style(token/): formatting

--- src/token/token.go
+++ src/token/token.go
@@ -27,10 +27,10 @@ const (
 	COMMA     = ","
 	SEMICOLON = ";"
 
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
 	LBRACKET = "["
 	RBRACKET = "]"
 
