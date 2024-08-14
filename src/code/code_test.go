94100e7bb97ce22e8c9ff0049b0289d2f0b8e1fb
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-13 23:09:21 -0400

    feat(code/): Add Add operator to the bytecode

--- src/code/code_test.go
+++ src/code/code_test.go
@@ -9,6 +9,7 @@ func TestMake(t *testing.T) {
 		expected []byte
 	}{
 		{OpConstant, []int{65_534}, []byte{byte(OpConstant), 255, 254}},
+		{OpAdd, []int{}, []byte{byte(OpAdd)}},
 	}
 
 	for _, tt := range tests {
@@ -30,14 +31,14 @@ func TestMake(t *testing.T) {
 
 func TestInstructionsString(t *testing.T) {
 	instructions := []Instructions{
-		Make(OpConstant, 1),
+		Make(OpAdd),
 		Make(OpConstant, 2),
 		Make(OpConstant, 65_535),
 	}
 
-	expected := `0000 OpConstant 1
-0003 OpConstant 2
-0006 OpConstant 65535
+	expected := `0000 OpAdd
+0001 OpConstant 2
+0004 OpConstant 65535
 `
 
 	concatted := Instructions{}
@@ -46,7 +47,7 @@ func TestInstructionsString(t *testing.T) {
 	}
 
 	if concatted.String() != expected {
-		t.Errorf("instructions wrongly formatted.\nwant=%q\ngo%q",
+		t.Errorf("instructions wrongly formatted.\nwant=%q\ngot =%q",
 			expected, concatted.String())
 	}
 }
