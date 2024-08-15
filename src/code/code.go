8c35062550cf9d876a2b1d9996e85d44fcec9a7f
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-15 14:22:57 -0400

    feat(compiler/): Add OpPop bytecode to the compiler and move some code

--- src/code/code.go
+++ src/code/code.go
@@ -6,52 +6,12 @@ import (
 	"fmt"
 )
 
-type Instructions []byte
-
-func (ins Instructions) String() string {
-	var out bytes.Buffer
-
-	i := 0
-	for i < len(ins) {
-		def, err := Lookup(ins[i])
-		if err != nil {
-			fmt.Fprintf(&out, "ERROR: %s\n", err)
-			continue
-		}
-
-		operants, read := ReadOperands(def, ins[i+1:])
-
-		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operants))
-
-		i += 1 + read
-	}
-
-	return out.String()
-}
-
-func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
-	operandCount := len(def.OperandWidths)
-
-	if len(operands) != operandCount {
-		return fmt.Sprintf("ERROR: operand len %d does not match defined %d",
-			len(operands), operandCount)
-	}
-
-	switch operandCount {
-	case 0:
-		return def.Name
-	case 1:
-		return fmt.Sprintf("%s %d", def.Name, operands[0])
-	}
-
-	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
-}
-
 type Opcode byte
 
 const (
 	OpConstant Opcode = iota
 	OpAdd
+	OpPop
 )
 
 type Definition struct {
@@ -62,6 +22,7 @@ type Definition struct {
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
 	OpAdd:      {"OpAdd", []int{}},
+	OpPop:      {"OpPop", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -118,4 +79,45 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
+}
+
+type Instructions []byte
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operants, read := ReadOperands(def, ins[i+1:])
+
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operants))
+
+		i += 1 + read
+	}
+
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d",
+			len(operands), operandCount)
+	}
+
+	switch operandCount {
+	case 0:
+		return def.Name
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
 }
