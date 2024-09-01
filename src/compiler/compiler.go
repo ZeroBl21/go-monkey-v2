0c32ae54308688ee96addc99f019683d61aaf47c
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-31 20:17:32 -0400

    feat(compiler/): Add symbol table implementation for variable declaration and accessing

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -19,6 +19,8 @@ type Compiler struct {
 
 	lastInstruction     EmittedInstruction
 	previousInstruction EmittedInstruction
+
+	symbolTable *SymbolTable
 }
 
 func New() *Compiler {
@@ -27,6 +29,7 @@ func New() *Compiler {
 		constants:           []object.Object{},
 		lastInstruction:     EmittedInstruction{},
 		previousInstruction: EmittedInstruction{},
+		symbolTable:         NewSymbolTable(),
 	}
 }
 
@@ -39,7 +42,21 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+		if !ok {
+			return fmt.Errorf("undefined variable %s", node.Value)
+		}
+		c.emit(code.OpGetGlobal, symbol.Index)
+
 	// Statements
+
+	case *ast.LetStatement:
+		if err := c.Compile(node.Value); err != nil {
+			return err
+		}
+		symbol := c.symbolTable.Define(node.Name.Value)
+		c.emit(code.OpSetGlobal, symbol.Index)
 
 	case *ast.ExpressionStatement:
 		if err := c.Compile(node.Expression); err != nil {
