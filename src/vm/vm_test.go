5228063d5a46fbdca08fb9c869d5cea3c7350d01
Darwin Perdomo <ZeroBl@hotmail.com>
2024-09-04 21:08:10 -0400

    fix(vm/): Fix test typos

--- src/vm/vm_test.go
+++ src/vm/vm_test.go
@@ -96,14 +96,14 @@ func TestHashLiterals(t *testing.T) {
 	tests := []vmTestCase{
 		{"{}", map[object.HashKey]int64{}},
 		{
-			"{1: 2, 3: 4}",
+			"{1: 2, 2: 3}",
 			map[object.HashKey]int64{
 				(&object.Integer{Value: 1}).HashKey(): 2,
-				(&object.Integer{Value: 3}).HashKey(): 4,
+				(&object.Integer{Value: 2}).HashKey(): 3,
 			},
 		},
 		{
-			"[1 + 1: 2 * 2:, 3 + 3: 4 * 4]",
+			"{1 + 1: 2 * 2, 3 + 3: 4 * 4}",
 			map[object.HashKey]int64{
 				(&object.Integer{Value: 2}).HashKey(): 4,
 				(&object.Integer{Value: 6}).HashKey(): 16,
