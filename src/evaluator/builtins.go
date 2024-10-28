7b148e39d2c89bb141c8644ac612eb9b5341b58f
Darwin Perdomo <ZeroBl@hotmail.com>
2024-10-28 15:13:48 -0400

    refactor(object/builtins.go): Organising builtins functions

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -6,11 +6,12 @@ import (
 
 // The built-in functions / standard-library methods are stored here.
 var builtins = map[string]*object.Builtin{
-	"len":        object.GetBuiltinByName("len"),
+	"len":   object.GetBuiltinByName("len"),
+	"puts":  object.GetBuiltinByName("puts"),
+	"first": object.GetBuiltinByName("first"),
+	"last":  object.GetBuiltinByName("last"),
+	"rest":  object.GetBuiltinByName("rest"),
+	"push":  object.GetBuiltinByName("push"),
+
 	"unicodeLen": object.GetBuiltinByName("unicodeLen"),
-	"first":      object.GetBuiltinByName("first"),
-	"last":       object.GetBuiltinByName("last"),
-	"rest":       object.GetBuiltinByName("rest"),
-	"push":       object.GetBuiltinByName("push"),
-	"puts":       object.GetBuiltinByName("puts"),
 }
