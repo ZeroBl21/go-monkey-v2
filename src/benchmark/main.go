2b5cd646430eea4ca8dc4a8b57eb9ae25eba638c
Darwin Perdomo <ZeroBl@hotmail.com>
2024-11-01 15:01:27 -0400

    feat(benchmark/): Add small benchmark between Interpreted and Compiled versions

---
+++ src/benchmark/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/ZeroBl21/go-monkey/src/compiler"
+	"github.com/ZeroBl21/go-monkey/src/evaluator"
+	"github.com/ZeroBl21/go-monkey/src/lexer"
+	"github.com/ZeroBl21/go-monkey/src/object"
+	"github.com/ZeroBl21/go-monkey/src/parser"
+	"github.com/ZeroBl21/go-monkey/src/vm"
+)
+
+var engine = flag.String("engine", "vm", "use 'vm' or 'eval'")
+
+const input = `
+let fibonacci = fn(x) {
+	if (x == 0) {
+		0
+	} else {
+		if (x == 1) {
+			return 1;
+		} else {
+			fibonacci(x - 1) + fibonacci(x - 2);
+		}
+	}
+};
+
+fibonacci(35);`
+
+func main() {
+	flag.Parse()
+
+	var duration time.Duration
+	var result object.Object
+
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if *engine == "vm" {
+		comp := compiler.New()
+		if err := comp.Compile(program); err != nil {
+			fmt.Printf("compiler error: %s", err)
+			return
+		}
+
+		machine := vm.New(comp.Bytecode())
+
+		start := time.Now()
+
+		if err := machine.Run(); err != nil {
+			fmt.Printf("vm error: %s", err)
+			return
+		}
+
+		duration = time.Since(start)
+		result = machine.LastPoppedStackElem()
+
+	} else {
+		env := object.NewEnvironment()
+
+		start := time.Now()
+
+		result = evaluator.Eval(program, env)
+
+		duration = time.Since(start)
+	}
+
+	fmt.Printf(
+		"engine=%s, result=%s, duration=%s\n",
+		*engine,
+		result.Inspect(),
+		duration,
+	)
+}
